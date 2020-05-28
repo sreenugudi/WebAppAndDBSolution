cf70a446064ba381a29caff32bc865386d13ab02
naman2710 <naman.verma1985@gmail.com>
2020-05-28 16:51:37 +0530

    sfd

---
+++ UnitTestProject/UnitTest1.cs
@@ -0,0 +1,33 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace UnitTestProject
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            var a = true;
+            Assert.IsTrue(a);
+        }
+        [TestMethod]
+        public void TestMethod2()
+        {
+            var a = true;
+            Assert.IsTrue(a);
+        }
+        [TestMethod]
+        public void TestMethod3()
+        {
+            var a = true;
+            Assert.IsTrue(a);
+        }
+        [TestMethod]
+        public void TestMethod4()
+        {
+            var a = true;
+            Assert.IsTrue(a);
+        }
+    }
+}
